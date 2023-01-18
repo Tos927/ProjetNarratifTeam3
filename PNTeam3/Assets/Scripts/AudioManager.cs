e8cdacec1fe118dad49bd53e7ac2194cd79111f9
Tos927 <t.sautereau92@gmail.com>
2023-01-18 14:49:39 +0100

    Maquette V3

--- PNTeam3/Assets/Scripts/AudioManager.cs
+++ PNTeam3/Assets/Scripts/AudioManager.cs
@@ -1,8 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class AudioManager : MonoBehaviour
-{    
+{
+    public static AudioManager instance;
 
+    public Sound[] musicSounds, sfxSounds, dubbingSounds;
+    public AudioSource musicSource, sfxSource, dubbingSource;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayMusic("MainTheme");
+    }
+
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(musicSounds, x => x.name == name);
+
+        if (s==null)
+        {
+            Debug.Log("Sound not found");
+        }
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+    }
+    public void PlaySFX(string name)
+    {
+
+        Sound s = Array.Find(sfxSounds, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sound not found");
+        }
+        else
+        {
+            sfxSource.PlayOneShot(s.clip);
+        }
+    }
+    public void PlayDubbing(string name) 
+    {
+        Sound s = Array.Find(dubbingSounds, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sound not found");
+        }
+        else
+        {
+            dubbingSource.PlayOneShot(s.clip);
+        }
+    }
+
+    public void ToggleMusic()
+    {
+        musicSource.mute= !musicSource.mute;
+    }
+
+    public void ToggleSFX()
+    {
+        sfxSource.mute= !sfxSource.mute;
+    }
+
+    public void ToggleDubbing()
+    {
+        dubbingSource.mute= !dubbingSource.mute;
+    }
+
+    public void MusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+    
+    public void SFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
+    }
+
+    public void DubbingVolume(float volume)
+    {
+        dubbingSource.volume = volume;
+    }
 }
