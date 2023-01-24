976564dcdcabf45443dc94a85ce16e356804e1bb
Corentin Chrisment <tintinch59@gmail.com>
2023-01-24 22:07:35 +0100

    [WIP] Setup first circle

--- PNTeam3/Assets/Proto1/Circledivider.cs
+++ PNTeam3/Assets/Proto1/Circledivider.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Circledivider : MonoBehaviour
 {
@@ -12,17 +15,27 @@ public class Circledivider : MonoBehaviour
     private void Start()
     {
         float angleStep = 360f / numberOfParts;
-        for (int i = 0; i <= numberOfParts; i++)
+        for (int i = 0; i < numberOfParts; ++i)
         {
             float angle = angleStep * i;
             Vector3 linePosition = new Vector3(transform.position.x, transform.position.y, 0);
 
             GameObject line = Instantiate(linePrefab, linePosition, Quaternion.identity);
-            line.GetComponent<RectTransform>().transform.Rotate(new Vector3(0,0,angle));
+            RectTransform lineRect = line.GetComponent<RectTransform>();
+            lineRect.transform.Rotate(new Vector3(0,0,-angle));
+            lineRect.sizeDelta = new Vector2(lineRect.rect.width, GetComponent<RectTransform>().rect.height);
             line.transform.SetParent(transform);
+            line.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
 
-            CodexRotation.endCodex += FindCurrentPart;
+            GameObject textPositionTest = Instantiate(textPrefab, transform.position, Quaternion.identity);
+            textPositionTest.transform.SetParent(transform);
+            line.transform.SetParent(transform);
+            RectTransform textRect = textPositionTest.GetComponent<RectTransform>();
+            textRect.sizeDelta = new Vector2(lineRect.rect.width * 5, GetComponent<RectTransform>().rect.height * .85f);
+            textRect.transform.Rotate(new Vector3(0, 0, -angle - angleStep / 2));
         }
+        CodexRotation.endCodex += FindCurrentPart;
+
     }
 
     private void FindCurrentPart()
